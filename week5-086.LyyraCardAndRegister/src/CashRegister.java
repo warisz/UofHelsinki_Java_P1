5554488ba94a2b370f093f19eca3d105fc50afb4
Waris Z <wariszahoor0@gmail.com>
2018-01-15 20:16:52 -0500

    Challenge 86

--- week5-086.LyyraCardAndRegister/src/CashRegister.java
+++ week5-086.LyyraCardAndRegister/src/CashRegister.java
@@ -7,16 +7,23 @@ public class CashRegister {
 
     public CashRegister() {
         // at start the register has 1000 euros 
+        this.cashInRegister=1000;
     }
 
     public double payEconomical(double cashGiven) {
         // price of the economical lunch is 2.50 euros
         // if the given cash is at least the price of the lunch:
+        if (cashGiven>=2.5){
+            economicalSold++;
+            this.cashInRegister+=2.5;
+            return cashGiven-2.5;
+        }
+        return cashGiven;
         //    the price of lunch is added to register
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
         // if not enough money given, all is returned and nothing else happens        
-        return -1;
+        
     }
 
     public double payGourmet(double cashGiven) {
@@ -26,9 +33,39 @@ public class CashRegister {
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
         // if not enough money given, all is returned and nothing else happens
-        return -1;
+        if (cashGiven>=4){
+                gourmetSold++;
+                this.cashInRegister+=4;
+                return cashGiven-4;
+            }
+            return cashGiven;
+ 
+    }
+    
+    public boolean payEconomical(LyyraCard card){
+        if (card.balance()>=2.5){
+            card.pay(2.5);
+            this.economicalSold++;
+            return true;
+        }
+        return false;
     }
 
+    public boolean payGourmet(LyyraCard card){
+        if (card.balance()>=4){
+            this.gourmetSold++;
+            card.pay(4);
+            return true;
+        }
+        return false;
+    }
+    
+    public void loadMoneyToCard(LyyraCard card, double sum){
+        if(sum>0){
+            card.loadMoney(sum);
+            this.cashInRegister+=sum;
+        }
+    }
 
     public String toString() {
         return "money in register " + cashInRegister + " economical lunches sold: " + economicalSold + " gourmet lunches sold: " + gourmetSold;
