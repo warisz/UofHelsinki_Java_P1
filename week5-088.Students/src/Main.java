5fb47c72dca622a6bc5e4c50029356b5c8880dc9
Waris Z <wariszahoor0@gmail.com>
2018-01-16 21:31:27 -0500

    Challenges 87 & 88

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -6,5 +6,40 @@ public class Main {
 
     public static void main(String[] args) {
         // write here the main program
+        
+        ArrayList<Student> list = new ArrayList<Student>();
+        
+        Scanner reader = new Scanner (System.in);
+        String name = ".";
+        String studentNumber = ".";
+        
+        while (true){
+            System.out.print("name: ");
+            name = reader.nextLine();
+            
+            if(name.equals("")){
+                break;
+            }
+            System.out.print("studentnumber:");
+            studentNumber = reader.nextLine();
+            
+            Student newStudent = new Student (name, studentNumber);
+            list.add(newStudent);
+    }
+        System.out.println("");
+        for(Student student : list){
+            System.out.println(student);
+        }
+        System.out.println("");
+        
+        System.out.print("Give search term: ");
+        String search = reader.nextLine();
+        
+        System.out.println("Result: ");
+        for(Student student : list){
+            if(student.getName().contains(search)){
+                System.out.println(student);
+            }
+        }
     }
 }
